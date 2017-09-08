58681626564fce31acadf39d75adc4e6d7a95575
Peter Zemianek <peteyorn>
2017-09-08 18:46:56 +0200

    testing

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/test', function(request, response) {
+  response.send('Hello from github man!');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
