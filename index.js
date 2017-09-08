97ad05a1ff306b2647080ad8c744ff3de98ac57c
Peter Zemianek <peteyorn>
2017-09-08 18:21:08 +0200

    remove cool

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,10 +11,6 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
 });
 
 app.listen(app.get('port'), function() {
