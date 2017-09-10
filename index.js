a1ce5fbf327c382dd59b46bfc5662d9059cf1c36
Peter Zemianek <peteyorn>
2017-09-10 22:21:38 +0200

    header

--- index.js
+++ index.js
@@ -25,9 +25,13 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function (request, response) {
-  response.render('pages/index');
+app.get('/', function (req, res) {
+  res.render('pages/index');
 });
+
+app.get('/customer', function (req, res) {
+  res.render('pages/customer');
+})
 
 //user routes
 app.post('/register', userApi.register);
