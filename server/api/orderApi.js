05d3bfb6543bb8c70a8bce12536157bce11ac53c
airen <patlevic.kristian@gmail.com>
2017-11-10 14:33:28 +0100

    added next into functions

--- server/api/orderApi.js
+++ server/api/orderApi.js
@@ -9,7 +9,7 @@ var readline = require('readline');
 var auth;
 
 module.exports = {
-calendar(req, res) {
+calendar(req, res, next) {
     fs.readFile('config/client_secret.json', function processClientSecrets(err, content) {
         if (err) {
             console.log('Error loading client secret file: ' + err);
@@ -24,7 +24,7 @@ calendar(req, res) {
     })
 },
 
-new(req, res) {
+new(req, res, next) {
     Order.getFormData(function(err, form) {
         if (!err)
             return res.render('pages/order/order-new', { result: {form: form} });
@@ -33,7 +33,7 @@ new(req, res) {
     })
 },
 
-create(req, res) {
+create(req, res, next) {
     var order = req.body.order;
 
     Order.create(order)
@@ -45,7 +45,7 @@ create(req, res) {
         })
 },
 
-get(req, res) {
+get(req, res, next) {
     Order.findById(req.params.id) 
         .then(order => {
             return res.render('pages/order/order', { order: order });
@@ -55,7 +55,7 @@ get(req, res) {
         })
 },
 
-getAll(req, res) {
+getAll(req, res, next) {
     Order.find({})
         .then(orders => {
             return res.render('pages/order/orders', { orders: orders });
@@ -81,7 +81,7 @@ edit(req,res) {
         })
 },
 
-/*edit(req, res) {
+/*edit(req, res, next) {
     Order.getDetail(req.params.id, function (err, detail) {
         if (err) {
             return next(err);
@@ -96,7 +96,7 @@ edit(req,res) {
     })
 },*/
 
-update(req, res) {
+update(req, res, next) {
     Order.findById(req.params.id)
         .then(order => {
             if (!order) {
@@ -114,7 +114,7 @@ update(req, res) {
         })
 },
 
-getDetail(req, res) {
+getDetail(req, res, next) {
     Order.getDetail(req.params.id)
         .then(result => {
             return res.send(result);
