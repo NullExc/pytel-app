db09e2d4763de207a243c79d53174506fad10df8
airen <patlevic.kristian@gmail.com>
2017-11-10 14:32:18 +0100

    reverting previous reverts

--- server/api/ordertypeApi.js
+++ server/api/ordertypeApi.js
@@ -1,32 +1,34 @@
 var OrderType = require('../models/OrderType');
 
-module.exports.create = function (req, res, next) {
-
+module.exports = {
+    
+create(req, res, next) {
     var orderType = req.body.ordertype;
 
-    OrderType.create( orderType, function (err, orderType) {
-        if (err) {
+    OrderType.create(orderType)
+        .then(orderType => {
+            return res.send({message: 'work type created'});
+        }) 
+        .catch(err => {
             return next(err);
-        }
-        res.send({message: 'work type created'});
-    })
-}
+        })
+},
 
-module.exports.get = function (req, res, next) {
+get(req, res, next) {
     OrderType.findById(req.params.id, function (err, orderType) {
         if (err) {
             return next(err);
         }
         res.send({orderType: orderType});
     })
-}
+},
 
-module.exports.getAll = function (req, res, next) {
-    
+getAll(req, res, next) {    
     OrderType.find( {}, function (err, orderTypes) {
         if (err) {
             return next(err);
         }
         res.send({orderTypes: orderTypes});
     })
+}
 }
